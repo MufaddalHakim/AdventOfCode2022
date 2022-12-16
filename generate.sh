8d76935de2eb8475dd4a45327fa30b64be50b1e9
Mufaddal Hakim <mufaddalhakim15@gmail.com>
2022-12-16 22:00:33 +0530

    get input data using curl

--- generate.sh
+++ generate.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+if [ -f .env ]
+then
+  export $(cat .env | xargs)
+fi
+
 read -d '' template << EOM
 const fs = require('fs');
 
 const input = fs.readFileSync(\`\${process.argv[2]}.txt\`, 'utf-8').split('\\\n');
 EOM
 
-mkdir ./$1
-echo "$template" > ./$1/p1.js
-echo "$template" > ./$1/p2.js
+mkdir ./day$1
+echo "$template" > ./day$1/p1.js
+echo "$template" > ./day$1/p2.js
 
-touch "./$1/test.txt"
-touch "./$1/input.txt"
+
+# Create input and test files
+touch "./day$1/test.txt"
+curl -s https://adventofcode.com/2022/day/$1/input --cookie "session=$session" > "./day$1/input.txt"
