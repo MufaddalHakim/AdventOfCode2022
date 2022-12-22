dd815acf4c34488b6963036abdb578a909e0b262
Mufaddal Hakim <mufaddalhakim15@gmail.com>
2022-12-22 11:46:35 +0530

    day 3 solved

--- day3/p1.js
+++ day3/p1.js
@@ -1,3 +1,31 @@
 const fs = require('fs');
 
 const input = fs.readFileSync(`${process.argv[2]}.txt`, 'utf-8').split('\n');
+
+let prioritySum = 0;
+for (let str of input) {
+  let h1 = str.slice(0, str.length/2);
+  let h2 = str.slice(str.length/2, str.length);
+  
+  let items1 = new Set();
+  for (let i = 0; i < h1.length; i++) {
+    items1.add(h1.charAt(i));
+  }
+
+  for (let i = 0; i < h2.length; i++) {
+    if (items1.has(h2.charAt(i))) {
+      // console.log(h2.charAt(i), getPriority(h2.charAt(i)));
+      prioritySum += getPriority(h2.charAt(i));
+      break;
+    }
+  }
+
+}
+
+function getPriority(ch) {
+  let ascii = ch.charCodeAt(0);
+  if (ascii > 97) return ascii - 96;
+  else return ascii - 38;
+}
+
+console.log(prioritySum);
--- day3/p2.js
+++ day3/p2.js
@@ -1,3 +1,37 @@
 const fs = require('fs');
 
 const input = fs.readFileSync(`${process.argv[2]}.txt`, 'utf-8').split('\n');
+
+let prioritySum = 0;
+for (let i = 0; i < input.length;) {
+  let str1 = input[i++];
+  let str2 = input[i++];
+  let str3 = input[i++];
+
+  let common1 = new Set();
+  for (let j = 0; j < str1.length; j++) {
+    common1.add(str1.charAt(j));
+  }
+
+  let common2 = new Set();
+  for (let j = 0; j < str2.length; j++) {
+    if (common1.has(str2.charAt(j))) {
+      common2.add(str2.charAt(j));
+    }
+  }
+
+  for (let j = 0; j < str3.length; j++) {
+    if (common2.has(str3.charAt(j))) {
+      prioritySum += getPriority(str3.charAt(j));
+      break;
+    }
+  }
+}
+
+function getPriority(ch) {
+  let ascii = ch.charCodeAt(0);
+  if (ascii > 97) return ascii - 96;
+  else return ascii - 38;
+}
+
+console.log(prioritySum);
