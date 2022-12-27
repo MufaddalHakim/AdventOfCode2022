2cb2fae68abf86b95c51600f694a99a0d3749b41
Mufaddal Hakim <mufaddalhakim15@gmail.com>
2022-12-27 23:45:48 +0530

    day 9 part 1 solved

---
+++ day9/p1.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+const input = fs.readFileSync(`${process.argv[2]}.txt`, 'utf-8').split('\n');
+input.pop();
+
+let head = [0, 0];
+let tail = [0, 0];
+let visited = new Set();
+
+for (let move of input) {
+  for (let i = 0; i < move.split(' ')[1]; i++) {
+    let dir = move.split(' ')[0];
+    switch (dir) {
+      case 'U':
+        head[1]++;
+        break;
+      case 'D':
+        head[1]--;
+        break;
+      case 'L':
+        head[0]--;
+        break;
+      case 'R':
+        head[0]++;
+        break;
+    }
+
+    tail = findTailPos(head, tail);
+    visited.add(tail.toString());
+    // console.log(move, head, tail);s
+  }
+}
+
+function findTailPos(head, tail) {
+  if (Math.abs(head[0] - tail[0]) > 1) {
+    if (head[0] > tail[0]) tail[0]++;
+    else tail[0]--;
+    tail[1] = head[1];
+  }
+  if (Math.abs(head[1] - tail[1]) > 1) {
+    if (head[1] > tail[1]) tail[1]++;
+    else tail[1]--;
+    tail[0] = head[0];
+  }
+  return tail;
+}
+
+console.log(visited.size);
